ddf6d5dd1c7d50ca80ed4d018157efb362868cc4
Kuldeep Gupta <kuldeep.gupta@thewasserstoff.com>
2024-08-29 15:09:35 +0530

    cors

--- server.js
+++ server.js
@@ -8,16 +8,10 @@ dotenv.config();
 connectDB();
 const app = express();
 app.use(express.json());
-app.use(
-	cors({
-		origin: process.env.ORIGIN,
-		methods: ["GET", "PUT", "POST", "DELETE"],
-		credentials: true,
-	})
-);
+app.use(cors());
 app.use(router);
-app.get('/', (req, res)=>{
-res.status(200).json({status:true , message:"api is working" })
+app.get('/', (req, res) => {
+	res.status(200).json({ status: true, message: "api is working" })
 })
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/auctions", require("./routes/auctionRoutes"));
